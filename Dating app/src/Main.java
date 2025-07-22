79555ce33276d9e66871d01c8b73f6c55c2e2c6b
bibhusan-69 <109571270+Bibhusan-dot@users.noreply.github.com>
2025-07-22 22:46:50 +0545

    Add files via upload

---
+++ Dating app/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        SimpleDate date = new SimpleDate(27,2,2006);
+        date.fuck();
+        date.advance(6);
+        date.fuck();
+    }
+}
---
+++ Dating app/src/SimpleDate.java
@@ -0,0 +1,57 @@
+public class SimpleDate {
+    private
+    int year;
+    int month;
+    int day;
+
+    public
+    SimpleDate(int day,int month,int year){
+        this.year = year;
+        this.month = month;
+        this.day = day;
+
+    }
+
+
+    public void fuck(){
+        System.out.println(day + "." + month + "." + year);
+    }
+
+    boolean before(SimpleDate compared){
+        if(this.year < compared.year){
+            return true;
+        }
+        if(this.year == compared.year && this.month < compared.month){
+            return true;
+        }
+        if(this.year == compared.year && this.month == compared.month && this.day < compared.day){
+            return true;
+        }
+
+        return false;
+    }
+
+
+    public void advance(){
+        this.day++;
+        if(this.day > 30){
+            this.month++;
+        }
+        if(this.month > 12){
+            this.year++;
+        }
+    }
+
+    public void advance(int howManyDays){
+        this.day = this.day + howManyDays;
+        if(this.day > 30){
+            this.day = day - 30;
+            this.month++;
+        }
+        if(this.month > 12){
+            month = month -12;
+            this.year++;
+        }
+    }
+
+}
