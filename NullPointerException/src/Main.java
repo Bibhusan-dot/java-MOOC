b7319ce8305ccff51eaeef067340b09117b66972
bibhusan-69 <109571270+Bibhusan-dot@users.noreply.github.com>
2025-07-22 22:50:12 +0545

    Add files via upload

---
+++ NullPointerException/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Null a = new Null();
+        a = null;
+        a.increase();
+    }
+}
+
+//we just tried to learn about null pointer exception about what happens when we assign a null
+// value to a object then we try to do somethin anything in it it will give error
---
+++ NullPointerException/src/Null.java
@@ -0,0 +1,9 @@
+public class Null {
+    private
+    int ans = 9;
+
+    public
+    int increase(){
+        return ans + 1;
+    }
+}
