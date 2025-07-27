71851e205ee0f78e199c468180971689ab25fbc5
bibhusan-69 <109571270+Bibhusan-dot@users.noreply.github.com>
2025-07-27 22:31:07 +0545

    Update Main.java

--- Dating app/src/Main.java
+++ Dating app/src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
 
         SimpleDate date = new SimpleDate(27,2,2006);
-        date.fuck();
+        date.out();
         date.advance(6);
-        date.fuck();
+        date.out();
     }
 }
